8cb79b702a10fada02c73062a6ff3095828faaa0
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-07-31 15:01:36 +0600

    Node_Monogo_Crud

--- node-mongo-crud.js
+++ node-mongo-crud.js
@@ -13,10 +13,19 @@ MongoClient.connect(URL, (error, MongoClientConnection) => {
     } else {
         console.log('Your Connection is Success');
 
-        insertData(MongoClientConnection);
+        // insertData(MongoClientConnection);
+
+        // deleteDataOne(MongoClientConnection);
+
+        // dataDeleteMany(MongoClientConnection);
+
+        // findOneItem(MongoClientConnection);
+
+        // findSpecificElement(MongoClientConnection);
+
+        findAllItem(MongoClientConnection);
     }
 })
-
 
 const insertData = (MongoClientConnection) => {
 
@@ -25,13 +34,105 @@ const insertData = (MongoClientConnection) => {
     const MyCollection = MyDataBaseConnection.collection("Students");
 
     // var myData = { "name": "SOHAN", "Roll": "01", "Class": "ten", "City": "Khulna" }
-    // const myData = { name: "OSMAN", age: "22", Roll: "02", Class: "Nini" }
+    const myData = { name: "RUMI", age: "33", Roll: "04", Class: "Five" }
 
     MyCollection.insertOne(myData, (error) => {
         if (error) {
             console.log('Data Inserted Failed');
         } else {
             console.log('Data Inserted Success');
+        }
+    })
+}
+
+
+const deleteDataOne = (MongoClientConnection) => {
+
+    const myDataBaseConnection = MongoClientConnection.db("School");
+
+    const MyCollection = myDataBaseConnection.collection("Students");
+
+    const deleteItem = { Roll: "01" }
+
+    MyCollection.deleteOne(deleteItem, (error) => {
+        if (error) {
+            console.log('Data Delete Failed');
+        } else {
+            console.log('Data Delete Success');
+        }
+    })
+}
+
+
+const dataDeleteMany = (MongoClientConnection) => {
+
+    const MyDataBaseConnection = MongoClientConnection.db("School");
+
+    const MyCollection = MyDataBaseConnection.collection("Students");
+
+    MyCollection.deleteMany((error) => {
+        if (error) {
+            console.log('Data Delete Failed');
+        } else {
+            console.log('Data Delete Success');
+        }
+    })
+}
+
+
+const findOneItem = (MongoClientConnection) => {
+
+    const MyDataBaseConnection = MongoClientConnection.db("School");
+
+    const MyCollection = MyDataBaseConnection.collection("Students");
+
+    const findItem = {};
+
+    MyCollection.findOne((error, result) => {
+
+        if (error) {
+            console.log(error)
+        } else if (result) {
+            console.log(result)
+        }
+    })
+}
+
+
+
+const findSpecificElement = (MongoClientConnection) => {
+
+
+    const MyDataBaseConnection = MongoClientConnection.db("School");
+
+    const MyConnection = MyDataBaseConnection.collection("Students");
+
+    const findSpecificItem = { Class: "Nine" }
+
+    MyConnection.findOne(findSpecificItem, (error, result) => {
+
+        if (error) {
+            console.log(error)
+        } else {
+            console.log(result)
+        }
+    })
+}
+
+
+
+const findAllItem = (MongoClientConnection) => {
+
+
+    const MyDataBaseConnection = MongoClientConnection.db("School");
+
+    const MyConnection = MyDataBaseConnection.collection("Students");
+
+    MyConnection.find().toArray((error, result) => {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log(result)
         }
     })
 }
